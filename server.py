c3c13444bde7bab46adb77291ef9a1a6ce80391c
Nick Krichevsky <njk828@gmail.com>
2015-09-15 20:51:50 -0400

    Clean up server.py

--- server.py
+++ server.py
@@ -11,20 +11,20 @@ class Main():
 	@cherrypy.expose
 	def index(self):
 		raise cherrypy.HTTPError(403)
+
 	@cherrypy.expose
-	def upload(self,**kwargs):
+	def upload(self, **kwargs):
 			cherrypy.request.body.process()
 			parts = cherrypy.request.params['file']
-			outFile = open(os.path.join(location,cherrypy.request.params['name']),'w')
+			outFile = open(os.path.join(location, cherrypy.request.params['name']),'w')
 			if parts != None:
 				for part in parts:
 					outFile.write(part.fullvalue())
 				outFile.close()
-				print 'written!'
 			else:
 				raise ValueError
 
-application = cherrypy.tree.mount(Main(),'/')
+application = cherrypy.tree.mount(Main(), '/')
 if __name__=='__main__':
 	cherrypy.server.socket_host='0.0.0.0'
 	cherrypy.engine.start()
