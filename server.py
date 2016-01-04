05f789bd43bd7cfc024eb31645f32319517f22fa
Nick Krichevsky <njk828@gmail.com>
2016-01-03 22:20:00 -0500

    Move name generation to the serverside

--- server.py
+++ server.py
@@ -2,11 +2,16 @@ import cherrypy
 import cgi
 from configReader import ConfigReader
 import os.path
-import tempfile
+import hashlib
+from time import time
 
-configReader = ConfigReader(name="serverConfig.txt")
+configReader = ConfigReader(name = "serverConfig.txt")
 keys = configReader.getKeys()
 location = keys['location']
+adjectivesFile = open("adjectives.py", "r")
+adjectives = [line.rstrip() for line in adjectivesFile]
+adjectivesFile.close()
+
 class Main():
 	@cherrypy.expose
 	def index(self):
@@ -14,13 +19,18 @@ class Main():
 
 	@cherrypy.expose
 	def upload(self, **kwargs):
+			timeBytes = bytes(str(time()))
+			timeHash = hashlib.md5(timeBytes).hexDigest()[0:6]
+			adjective = random.choice(adjectives)
+			name = adjective + timeHash + ".png"
 			cherrypy.request.body.process()
 			parts = cherrypy.request.params['file']
-			outFile = open(os.path.join(location, cherrypy.request.params['name']),'w')
+			outFile = open(os.path.join(location, name),'w')
 			if parts != None:
 				for part in parts:
 					outFile.write(part.fullvalue())
 				outFile.close()
+				return name
 			else:
 				raise ValueError
 
